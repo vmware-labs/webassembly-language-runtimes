28756ad7c2e5018ebc4c776caa9f2003afac4189
Asen Alexandrov <alexandrov@vmware.com>
2022-11-22 19:45:51 +0200

    Add prototype support for php server on wasmedge

--- php/php-7.4.32/wl-build.sh
+++ php/php-7.4.32/wl-build.sh
@@ -15,28 +15,29 @@ export LDFLAGS_WASI="--sysroot=${WASI_SYSROOT} -lwasi-emulated-getpid -lwasi-emu
 export CFLAGS_SQLITE='-DSQLITE_OMIT_LOAD_EXTENSION=1'
 export LDFLAGS_SQLITE='-lsqlite3'
 
-export CFLAGS_PHP='-D_POSIX_SOURCE=1 -D_GNU_SOURCE=1 -DHAVE_FORK=0 -DWASM_WASI'
+export CFLAGS_PHP='-D_POSIX_SOURCE=1 -D_GNU_SOURCE=1 -DHAVE_FORK=0 -DWASM_WASI -DWASM_WASMEDGE'
 
-# We need to add LDFLAGS ot CFLAGS because autoconf compiles(+links) to binary when checking stuff
-export LDFLAGS="${LDFLAGS_WASI} ${LDFLAGS_DEPENDENCIES} ${LDFLAGS_SQLITE}"
-export CFLAGS="${CFLAGS_CONFIG} ${CFLAGS_WASI} ${CFLAGS_SQLITE} ${CFLAGS_DEPENDENCIES} ${CFLAGS_PHP} ${LDFLAGS}"
+# We need to add LDFLAGS to CFLAGS because autoconf compiles(+links) to binary when checking stuff
+export LDFLAGS="${LDFS} ${LDFLAGS_WASI} ${LDFLAGS_DEPENDENCIES} ${LDFLAGS_SQLITE} ${LDFLAGS}"
+export CFLAGS="${CFS} ${CFLAGS_CONFIG} ${CFLAGS_WASI} ${CFLAGS_SQLITE} ${CFLAGS_DEPENDENCIES} ${CFLAGS_PHP} ${LDFLAGS} ${CFLAGS}"
 
 cd "${WASMLABS_CHECKOUT_PATH}"
 
 logStatus "Generating configure script... "
 ./buildconf --force || exit 1
 
-export PHP_CONFIGURE=' --without-libxml --disable-dom --without-iconv --without-openssl --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter --without-pear --disable-phar --disable-opcache --disable-zend-signals --without-pcre-jit --with-sqlite3 --enable-pdo --with-pdo-sqlite'
+export PHP_CONFIGURE='--with-wasmedge --without-libxml --disable-dom --without-iconv --without-openssl --disable-simplexml --disable-xml --disable-xmlreader --disable-xmlwriter --without-pear --disable-phar --disable-opcache --disable-zend-signals --without-pcre-jit --with-sqlite3 --enable-pdo --with-pdo-sqlite'
 
 logStatus "Configuring build with '${PHP_CONFIGURE}'... "
 ./configure --host=wasm32-wasi host_alias=wasm32-musl-wasi --target=wasm32-wasi target_alias=wasm32-musl-wasi ${PHP_CONFIGURE} || exit 1
 
 logStatus "Building php-cgi... "
-make cgi || exit 1
+make cgi cli || exit 1
 
 logStatus "Preparing artifacts... "
 mkdir -p ${WASMLABS_OUTPUT}/bin 2>/dev/null || exit 1
 
 cp sapi/cgi/php-cgi ${WASMLABS_OUTPUT}/bin/ || exit 1
+cp sapi/cli/php ${WASMLABS_OUTPUT}/bin/ || exit 1
 
 logStatus "DONE. Artifacts in ${WASMLABS_OUTPUT}"
