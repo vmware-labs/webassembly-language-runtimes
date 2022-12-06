f6b11151b66f9d6e54f950233001174836ea561e
Asen Alexandrov <alexandrov@vmware.com>
2022-12-06 17:42:11 +0200

    build: Support building without patches

--- scripts/wl-setup-repo.sh
+++ scripts/wl-setup-repo.sh
@@ -11,10 +11,13 @@ then
     cd ${WASMLABS_CHECKOUT_PATH} || exit 1
     git config user.email "Wasm Labs Team"
     git config user.name "no-reply@wasmlabs.dev"
-    for p in ${WASMLABS_ENV}/patches/*.patch
-    do
-        git am --no-gpg-sign $p || exit 1
-    done
+    if [ -d ${WASMLABS_ENV}/patches/ ]
+    then
+        for p in ${WASMLABS_ENV}/patches/*.patch
+        do
+            git am --no-gpg-sign $p || exit 1
+        done
+    fi
 else
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo "!!! Reusing previous contents of ${WASMLABS_CHECKOUT_PATH} "
