22a4641cd0570e9ccfc7d6660d1d67b0a61f0ec4
Rafael Fernández López <rfernandezl@vmware.com>
2023-01-16 16:55:15 +0100

    feature: Add Ruby 3.2.0

---
+++ ruby/wl-env-repo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "--unset" ]]
+then
+    unset WASMLABS_REPO
+    unset WASMLABS_REPO_NAME
+    return
+fi
+
+export WASMLABS_REPO=https://github.com/ruby/ruby.git
+export WASMLABS_REPO_NAME=ruby
