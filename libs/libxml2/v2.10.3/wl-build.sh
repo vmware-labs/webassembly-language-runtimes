2109cab20f912e98f1816230925b4659cb5ebdd3
Jesús González <jesusgm@vmware.com>
2023-02-01 16:24:04 +0100

    chore: Removing unnecessary `CFLAGS` and `LDFLAGS`

--- libs/libxml2/v2.10.3/wl-build.sh
+++ libs/libxml2/v2.10.3/wl-build.sh
@@ -9,14 +9,14 @@ fi
 # export CFLAGS_CONFIG="-O3 -g"
 export CFLAGS_CONFIG="-O0"
 
-export CFLAGS_WASI="--sysroot=${WASI_SYSROOT} -I./wasmlabs-stubs -D_WASI_EMULATED_MMAN -D_WASI_EMULATED_GETPID -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS"
-export LDFLAGS_WASI="--sysroot=${WASI_SYSROOT} -lwasi-emulated-mman -lwasi-emulated-getpid -lwasi-emulated-signal -lwasi-emulated-process-clocks"
+export CFLAGS_WASI="--sysroot=${WASI_SYSROOT} -I./wasmlabs-stubs"
+export LDFLAGS_WASI="--sysroot=${WASI_SYSROOT}"
 
 export CFLAGS_LIBXML2=''
 
 logStatus "Using LIBXML2 DEFINES: ${CFLAGS_LIBXML2}"
 
-export CFLAGS_BUILD='-D_POSIX_SOURCE=1 -D_GNU_SOURCE=1 -DHAVE_FORK=0 -DWASM_WASI'
+export CFLAGS_BUILD=''
 
 # We need to add LDFLAGS ot CFLAGS because autoconf compiles(+links) to binary when checking stuff
 export CFLAGS="${CFLAGS_CONFIG} ${CFLAGS_WASI} ${CFLAGS_LIBXML2} ${CFLAGS_BUILD} ${LDFLAGS_WASI}"
