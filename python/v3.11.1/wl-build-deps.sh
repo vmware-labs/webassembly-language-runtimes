96816a378b2e419b42480f6010f717e120098d6c
Asen Alexandrov <alexandrov@vmware.com>
2023-02-15 00:33:14 +0200

    fix: GH actions and deps

--- python/v3.11.1/wl-build-deps.sh
+++ python/v3.11.1/wl-build-deps.sh
@@ -14,21 +14,21 @@ fi
 logStatus "Building dependencies... "
 
 ### uuid
-$WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/uuid/libuuid-1.0.3 || exit 1
+env -u WASMLABS_BUILD_FLAVOR $WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/uuid/libuuid-1.0.3 || exit 1
 
 export CFLAGS_DEPENDENCIES="-I${WASMLABS_OUTPUT_BASE}/uuid/libuuid-1.0.3/include ${CFLAGS_DEPENDENCIES}"
 export LDFLAGS_DEPENDENCIES="-L${WASMLABS_OUTPUT_BASE}/uuid/libuuid-1.0.3/lib ${LDFLAGS_DEPENDENCIES}"
 
 
 ### zlib
-$WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/zlib/v1.2.13 || exit 1
+env -u WASMLABS_BUILD_FLAVOR $WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/zlib/v1.2.13 || exit 1
 
 export CFLAGS_DEPENDENCIES="-I${WASMLABS_OUTPUT_BASE}/zlib/v1.2.13/include ${CFLAGS_DEPENDENCIES}"
 export LDFLAGS_DEPENDENCIES="-L${WASMLABS_OUTPUT_BASE}/zlib/v1.2.13/lib ${LDFLAGS_DEPENDENCIES}"
 
 
 ### sqlite3
-$WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/sqlite/version-3.39.2 || exit 1
+env -u WASMLABS_BUILD_FLAVOR $WASMLABS_MAKE ${WASMLABS_REPO_ROOT}/libs/sqlite/version-3.39.2 || exit 1
 
 export CFLAGS_DEPENDENCIES="-I${WASMLABS_OUTPUT_BASE}/sqlite/version-3.39.2/include ${CFLAGS_DEPENDENCIES}"
 export LDFLAGS_DEPENDENCIES="-L${WASMLABS_OUTPUT_BASE}/sqlite/version-3.39.2/lib ${LDFLAGS_DEPENDENCIES}"
