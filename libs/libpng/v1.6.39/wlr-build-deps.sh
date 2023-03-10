13a5f2e83eee65cb20152838b987772f8d048104
Asen Alexandrov <alexandrov@vmware.com>
2023-03-10 17:20:44 +0200

    chore: Update libpng to consume libz from vmware-labs

--- libs/libpng/v1.6.39/wlr-build-deps.sh
+++ libs/libpng/v1.6.39/wlr-build-deps.sh
@@ -8,5 +8,5 @@ fi
 source ${WLR_REPO_ROOT}/scripts/build-helpers/wlr_dependencies.sh
 
 wlr_dependencies_add "zlib" "libs/zlib/v1.2.13" "lib/wasm32-wasi/libz.a" \
-    "https://github.com/assambar/webassembly-language-runtimes/releases/download/libs%2Fzlib%2F1.2.13%2B20230306-764c74d/libz-1.2.13-wasi-sdk-19.0.tar.gz"
+    "https://github.com/vmware-labs/webassembly-language-runtimes/releases/download/libs%2Fzlib%2F1.2.13%2B20230310-c46e363/libz-1.2.13-wasi-sdk-19.0.tar.gz"
 
