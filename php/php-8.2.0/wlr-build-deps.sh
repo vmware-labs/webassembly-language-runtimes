dd1fe52f0be52ac61e7aaf392a523f094bd1ba3e
Asen Alexandrov <alexandrov@vmware.com>
2023-04-04 18:00:29 +0300

    chore: Update sqlite3 dependents from 3.39.2 to 3.41.2

--- php/php-8.2.0/wlr-build-deps.sh
+++ php/php-8.2.0/wlr-build-deps.sh
@@ -22,8 +22,8 @@ wlr_dependencies_add "libxml2" "libs/libxml2/v2.10.3" "lib/wasm32-wasi/libxml2.a
 wlr_dependencies_add "oniguruma" "libs/oniguruma/v6.9.8" "lib/wasm32-wasi/libonig.a" \
     "https://github.com/vmware-labs/webassembly-language-runtimes/releases/download/libs%2Foniguruma%2F6.9.8%2B20230310-c46e363/libonig-6.9.8-wasi-sdk-19.0.tar.gz"
 
-wlr_dependencies_add "SQLite" "libs/sqlite/version-3.39.2" "lib/wasm32-wasi/libsqlite3.a" \
-    "https://github.com/vmware-labs/webassembly-language-runtimes/releases/download/libs%2Fsqlite%2F3.39.2%2B20230310-c46e363/libsqlite-3.39.2-wasi-sdk-19.0.tar.gz"
+wlr_dependencies_add "SQLite" "libs/sqlite/version-3.41.2" "lib/wasm32-wasi/libsqlite3.a" \
+    "https://github.com/vmware-labs/webassembly-language-runtimes/releases/download/libs%2Fsqlite%2F3.41.2%2B20230329-43f9aea/libsqlite-3.41.2-wasi-sdk-19.0.tar.gz"
 
 logStatus "Completed building dependencies for PHP 8.2.0!"
 logStatus " -> Exported PKG_CONFIG_PATH=${PKG_CONFIG_PATH}"
