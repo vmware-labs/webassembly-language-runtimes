45cca74ad2f4881d77325c5afcf9156f557cb4aa
Rafael Fernández López <rfernandezl@vmware.com>
2023-04-20 10:05:26 +0200

    Initial implementation of PHP on top of proxy-wasm

---
+++ php/proxy-wasm-bindings/wl-env-repo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "--unset" ]]
+then
+    unset WASMLABS_REPO
+    unset WASMLABS_REPO_NAME
+    return
+fi
+
+export WASMLABS_REPO=https://github.com/php/php-src.git
+export WASMLABS_REPO_NAME=php
+export WASMLABS_REPO_BRANCH=master
