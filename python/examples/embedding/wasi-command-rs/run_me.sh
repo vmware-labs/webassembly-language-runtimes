aa0ec248c0c5ef8069c5fe399f98a41ffe8a2bef
Asen Alexandrov <alexandrov@vmware.com>
2023-06-05 14:06:24 +0300

    chore: Add run_me.cmd for Windows

--- python/examples/embedding/wasi-command-rs/run_me.sh
+++ python/examples/embedding/wasi-command-rs/run_me.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+if ! (rustup target list | grep "installed" | grep -q "wasm32-wasi"); then
+    echo "Please run 'rustup target add wasm32-wasi' first"
+    exit 1
+fi
+
 set -e
 
-cargo build --target=wasm32-wasi
+PYO3_NO_PYTHON=1 cargo build --target=wasm32-wasi
 wasmtime \
     --mapdir /usr::target/wasm32-wasi/wasi-deps/usr \
     target/wasm32-wasi/debug/wasi-command-rs.wasm
