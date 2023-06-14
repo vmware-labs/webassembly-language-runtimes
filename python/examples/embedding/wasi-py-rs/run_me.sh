ff896882c4278af49a8b9b65af6bbfda12dc20c8
Asen Alexandrov <alexandrov@vmware.com>
2023-06-14 14:08:53 +0300

    WIP: not working, emulated libs not linked

--- python/examples/embedding/wasi-py-rs/run_me.sh
+++ python/examples/embedding/wasi-py-rs/run_me.sh
@@ -11,14 +11,14 @@ PYO3_NO_PYTHON=1 cargo build --target=wasm32-wasi
 echo -e "\n\033[35mCalling a WASI Command which embeds Python (adding a custom module implemented in Rust) and calls a custom function:\033[0m"
 set -x
 wasmtime \
-    --mapdir /usr::target/wasm32-wasi/wasi-deps/usr \
+    --mapdir /usr::../../../tools/wlr-libpy-link/target/wasm32-wasi/wasi-deps/usr \
     target/wasm32-wasi/debug/py-func-caller.wasm
 set +x
 
 echo -e "\n\033[35mCalling a WASI Command which wraps the Python binary (adding a custom module implemented in Rust):\033[0m"
 set -x
 wasmtime \
-    --mapdir /usr::target/wasm32-wasi/wasi-deps/usr \
+    --mapdir /usr::../../../tools/wlr-libpy-link/target/wasm32-wasi/wasi-deps/usr \
     target/wasm32-wasi/debug/py-wrapper.wasm \
     -- -c \
     'import person as p; pp = [p.Person("a", 1), p.Person("b", 2)]; pp[0].add_tag("X"); print("Filtered: ", p.filter_by_tag(pp, "X"))'
