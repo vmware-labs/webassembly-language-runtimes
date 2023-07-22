df4322845d6c2b04b288ee7342d04d28a1aeaf40
Gentle <ramon.klass@gmail.com>
2023-07-22 20:19:36 +0200

    feat: try to find actual target directory to place wasi deps into

--- python/tools/wlr-libpy/src/bld_cfg.rs
+++ python/tools/wlr-libpy/src/bld_cfg.rs
@@ -15,20 +15,32 @@ const LIBPYTHON_URL: &str = "https://github.com/vmware-labs/webassembly-language
 pub fn configure_static_libs() -> Result<LibsConfig, BoxedError> {
     let mut libs_config = LibsConfig::new();
 
-    let wasi_deps_path = Path::new(WASI_DEPS_PATH);
+    let out_dir = if let Ok(mut out_dir) = std::env::var("OUT_DIR") {
+        if let Some(position) = out_dir.rfind("/wasm32-wasi/") {
+            out_dir.truncate(position + 13);
+            out_dir.push_str("wasi-deps");
+            Some(out_dir)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+    .unwrap_or_else(|| WASI_DEPS_PATH.into());
+    let wasi_deps_path = Path::new(&out_dir);
 
     download_asset(WASI_SDK_SYSROOT_URL, wasi_deps_path)?;
-    libs_config.add_lib_path(format!("{WASI_DEPS_PATH}/wasi-sysroot/lib/wasm32-wasi"));
+    libs_config.add_lib_path(format!("{out_dir}/wasi-sysroot/lib/wasm32-wasi"));
     libs_config.add("wasi-emulated-signal");
     libs_config.add("wasi-emulated-getpid");
     libs_config.add("wasi-emulated-process-clocks");
 
     download_asset(WASI_SDK_CLANG_BUILTINS_URL, wasi_deps_path)?;
-    libs_config.add_lib_path(format!("{WASI_DEPS_PATH}/lib/wasi"));
+    libs_config.add_lib_path(format!("{out_dir}/lib/wasi"));
     libs_config.add("clang_rt.builtins-wasm32");
 
     download_asset(LIBPYTHON_URL, wasi_deps_path)?;
-    libs_config.add_lib_path(format!("{WASI_DEPS_PATH}/lib/wasm32-wasi"));
+    libs_config.add_lib_path(format!("{out_dir}/lib/wasm32-wasi"));
     libs_config.add("python3.11");
 
     Ok(libs_config)
